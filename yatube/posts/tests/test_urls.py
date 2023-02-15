789cbd9db6029af8868dcf464f4a7da923022a18
Александр Шпара <aleksandrshpr@yandex.ru>
2023-02-15 23:43:20 +0100

    push

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
-from django.urls import reverse
 
 from ..models import Group, Post
 
